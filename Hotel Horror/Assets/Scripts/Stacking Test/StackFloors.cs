d493e237399b179b3a596281d48ca087aaea7d6f
SeanDoc77 <59904251+SeanDoc77@users.noreply.github.com>
2020-01-15 16:00:12 -0500

    Completes the hotel generating

--- Hotel Horror/Assets/Scripts/Stacking Test/StackFloors.cs
+++ Hotel Horror/Assets/Scripts/Stacking Test/StackFloors.cs
@@ -11,11 +11,16 @@ public class StackFloors : MonoBehaviour
     public int minFloorCount;
     public int maxFloorCount;
     public float floorHeight;
-
     void Start()
     {
-        Instantiate(mainFloor);
-        Instantiate(secondFloor, new Vector3(0f, floorHeight, 0f), Quaternion.identity);
+        //Declares the Y value to instatiate the main floor
+        float spawnHeight = floorHeight / 2;
+
+        //Instantiates the main and second floor prefabs
+        Instantiate(mainFloor, new Vector3(0f, spawnHeight, 0f), Quaternion.identity);
+        Instantiate(secondFloor, new Vector3(0f, spawnHeight + floorHeight, 0f), Quaternion.identity);
+        //Instantiates the remaing guest floors
+        addGuestFloors();
     }
 
     // Update is called once per frame
@@ -27,6 +32,19 @@ public class StackFloors : MonoBehaviour
     //Function that generates create a random amout of floors above the second floor
     private void addGuestFloors()
     {
+        //Declares the Y value offset instatiate new floors
+        float spawnHeight = floorHeight / 2;
+
+        //Creates a random integer between (minFloorCount - 2) and (maxFloorCount - 2)
+        int guestFloors = Random.Range(minFloorCount, maxFloorCount)-2;
+
+        //Create a floor counter starting with 2 which represents the first 2 floors
+        int floorCount = 2;
+        for (int i=0; i<guestFloors; i++)
+        {
+            Instantiate(guestFloor, new Vector3(0f, spawnHeight + floorHeight*floorCount, 0f), Quaternion.identity);
+            floorCount++;
+        }
 
     }
 }
