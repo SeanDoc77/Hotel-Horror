516b1014081eee4616479c6bc41b133703bba5c4
SeanDoc77 <59904251+SeanDoc77@users.noreply.github.com>
2020-01-15 15:37:01 -0500

    Adds hotel generator

--- Hotel Horror/Assets/Scripts/Stacking Test/StackFloors.cs
+++ Hotel Horror/Assets/Scripts/Stacking Test/StackFloors.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 public class StackFloors : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject mainFloor;
+    public GameObject secondFloor;
+    public GameObject guestFloor;
+    public int minFloorCount;
+    public int maxFloorCount;
+    public float floorHeight;
+
     void Start()
     {
-        
+        Instantiate(mainFloor);
+        Instantiate(secondFloor, new Vector3(0f, floorHeight, 0f), Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    //Function that generates create a random amout of floors above the second floor
+    private void addGuestFloors()
+    {
+
     }
 }
