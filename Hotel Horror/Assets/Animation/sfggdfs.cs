93b8a0c592f9e5ad6d5d51db76dbfc41c78b9d66
CRGamesDirect <59904225+CRGamesDirect@users.noreply.github.com>
2020-01-16 17:17:51 -0500

    Laundry room

---
+++ Hotel Horror/Assets/Animation/sfggdfs.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sfggdfs : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Animation>().Play("Test");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
