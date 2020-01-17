7ebe0d2dd52851affc37de44157ddcb029e50ca4
SeanDoc77 <59904251+SeanDoc77@users.noreply.github.com>
2020-01-17 12:53:08 -0500

    Adds seed generator and procedural hotel contructor scripts

---
+++ Hotel Horror/Assets/Scripts/Hotel Generation/Procedural Hotel/HotelConstructor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotelConstructor : MonoBehaviour
+{
+    //Creates new seed object
+    HotelSeed hotelSeed = new HotelSeed();
+    int seed;
+    void Start()
+    {
+        //Generates new seed
+        hotelSeed.generateSeed();
+        seed = HotelSeed.seed;
+    }
+}
---
+++ Hotel Horror/Assets/Scripts/Hotel Generation/Procedural Hotel/HotelSeed.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotelSeed
+{
+    //Global seed variable
+    public static int seed;
+
+    public void generateSeed()
+    {
+        //Create a random int between 0 and 9999999999
+        int rand = Random.Range(0, 31622);
+
+        //Sets global seed varible to the random seed
+        HotelSeed.seed = rand;
+    }
+}
