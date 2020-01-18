85d154bcd5afa2bbcfc71dc8e173e393e8242ef2
SeanDoc77 <59904251+SeanDoc77@users.noreply.github.com>
2020-01-17 20:31:33 -0500

    Comments out a line in light flicker animation

--- Hotel Horror/Assets/Scripts/Hotel Generation/Procedural Hotel/HotelConstructor.cs
+++ Hotel Horror/Assets/Scripts/Hotel Generation/Procedural Hotel/HotelConstructor.cs
@@ -8,22 +8,25 @@ public class HotelConstructor : MonoBehaviour
     public int minFloors;
     public int maxFloors;
 
-    //Creates new seed object
-    HotelSeed hotelSeed = new HotelSeed();
+    HotelSeed hotelSeed = new HotelSeed(); //Creates new seed object
+
     int seed;
+    int addFloors; //Number of floors to add
+    
     void Start()
     {
-        //Generates new seed
-        hotelSeed.generateSeed();
+        hotelSeed.generateSeed(); //Generates new seed
         seed = HotelSeed.seed;
-        determineFloorsToAdd(2129857807, minFloors, maxFloors);
+
+        addFloors = determineFloorsToAdd(seed, minFloors, maxFloors);
         Debug.Log(seed);
     }
 
+    //Method that determines how many foors to add
     private int determineFloorsToAdd(int seed, int minFloors, int maxFloors)
     {
         int floorAmount;
-        int digits = 2; //Number of digits to pick from middle of seed
+        int digits = 3; //Number of digits to pick from middle of seed
 
         float max = (float)maxFloors-(float)(minFloors); //Number of floors that can potentially be added
         string seedString = seed.ToString(); //Converts the seed int to a string
@@ -32,12 +35,17 @@ public class HotelConstructor : MonoBehaviour
         //Calculate a percentage using the middle two digit of the seed
         string subString = seedString.Substring((int)(stringLength/2) - (int)(digits/2),  digits); //Gets the middle two digits of the seed
         float output = Int32.Parse(subString);//Converts the middle two digits to a float
-        float percentage = (output / 100); //Divides the 2 digit output by 100 to generate a percentage
+        float percentage = (float)(output / Math.Pow(10, digits)); //Divides the 2 digit output by 100 to generate a percentage
         float amount = percentage * max; //Multiplies the max potential floors by the percentage
         floorAmount = Mathf.RoundToInt(amount); // *maxFloors;
 
-
         Debug.Log(floorAmount);
         return floorAmount;
+    }
+
+    //Method that creates a grid of empty game objects that will be used to add rooms
+    private void createRoomGrid(int minFLoors, int addFloors)
+    {
+
     }
 }
